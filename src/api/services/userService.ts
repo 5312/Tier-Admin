82f4660f2560a4290ff2bc8fcc3eb8d5bb3ec5a3
5312 <1765080897@qq.com>
2025-06-27 14:30:31 +0800

    fix: 移除退出登录接口service

--- src/api/services/userService.ts
+++ src/api/services/userService.ts
@@ -15,19 +15,17 @@ export type SignInRes = UserToken & { user: UserInfo };
 export enum UserApi {
 	SignIn = "/v1/login",
 	SignUp = "/v1/register",
-	Logout = "/auth/logout",
 	Refresh = "/auth/refresh",
 	User = "/user",
 }
 
 const signin = (data: SignInReq) => apiClient.post<SignInRes>({ url: UserApi.SignIn, data });
 const signup = (data: SignUpReq) => apiClient.post<SignInRes>({ url: UserApi.SignUp, data });
-const logout = () => apiClient.get({ url: UserApi.Logout });
+
 const findById = (id: string) => apiClient.get<UserInfo[]>({ url: `${UserApi.User}/${id}` });
 
 export default {
 	signin,
 	signup,
 	findById,
-	logout,
 };
